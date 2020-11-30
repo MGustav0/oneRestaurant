c636eecf0d5bcbb7449a323919219e4c800ade37
Gustavo Moreira <18315899+MGustav0@users.noreply.github.com>
2020-11-30 18:25:36 -0500

    Manage configs

--- api/jest.config.ts
+++ api/jest.config.ts
@@ -69,7 +69,8 @@ export default {
 
   // An array of directory names to be searched recursively up from the requiring module's location
   // moduleDirectories: [
-  //   "node_modules"
+  //   "node_modules",
+  //   "src"
   // ],
 
   // An array of file extensions your modules use
